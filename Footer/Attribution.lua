9bc353edf2bc9c845df2da41844ab34654cdc64e
murpy <riggityroogityroorig@gmail.com>
2026-08-18 23:41:53 -0700

    Init patches, Acquire method, Attribution UI update

--- Footer/Attribution.lua
+++ Footer/Attribution.lua
@@ -1,9 +1,9 @@
-local function Init(SafeUi, Cloneref)
+local function Init(SafeUi, Cloneref, Interpreter)
     local ScreenGui = Instance.new("ScreenGui")
     ScreenGui.Parent = SafeUi
     ScreenGui.Name = ""
+    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     ScreenGui.IgnoreGuiInset = true
-    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
 
     local Frame = Instance.new("Frame")
     Frame.Parent = ScreenGui
@@ -16,30 +16,19 @@ local function Init(SafeUi, Cloneref)
     Frame.Name = ""
     Frame.BorderMode = Enum.BorderMode.Outline
     Frame.SizeConstraint = Enum.SizeConstraint.RelativeXY
-    Frame.ZIndex = 1
+    Frame.ZIndex = 2
     Frame.BorderSizePixel = 0
     Frame.Visible = true
-    Frame.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+    Frame.BackgroundColor3 = Color3.fromRGB(43, 43, 43)
     Frame.BackgroundTransparency = 1
     Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
     Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
-    Frame.Size = UDim2.new(0.24464832246303558, 0, 0.33557048439979553, 0)
-
-    local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-    UIAspectRatioConstraint.Name = ""
-    UIAspectRatioConstraint.Parent = Frame
-    UIAspectRatioConstraint.AspectRatio = 2
-
-    local UIGradient = Instance.new("UIGradient")
-    UIGradient.Parent = Frame
-    UIGradient.Rotation = 0
-    UIGradient.Name = ""
-    UIGradient.Rotation = 0
+    Frame.Size = UDim2.new(0.24464832246303558, 0, 0.4194630980491638, 0)
 
     local UICorner = Instance.new("UICorner")
     UICorner.Parent = Frame
     UICorner.Name = ""
-    UICorner.CornerRadius = UDim.new(0.03999999538064003, 0)
+    UICorner.CornerRadius = UDim.new(0.03200000151991844, 0)
 
     local UIShadow = Instance.new("UIShadow")
     UIShadow.Parent = Frame
@@ -48,8 +37,16 @@ local function Init(SafeUi, Cloneref)
     UIShadow.BlurRadius = UDim.new(0, 20)
     UIShadow.Transparency = 1
 
+    local UIStroke = Instance.new("UIStroke")
+    UIStroke.Parent = Frame
+    UIStroke.Name = ""
+    UIStroke.ZIndex = 3
+    UIStroke.Color = Color3.fromRGB(30, 30, 30)
+    UIStroke.Thickness = 3
+    UIStroke.Transparency = 1
+
     local ImageLabel = Instance.new("ImageLabel")
-    ImageLabel.Parent = ScreenGui
+    ImageLabel.Parent = Frame
     ImageLabel.LayoutOrder = 0
     ImageLabel.Active = false
     ImageLabel.Interactable = true
@@ -59,42 +56,37 @@ local function Init(SafeUi, Cloneref)
     ImageLabel.Name = ""
     ImageLabel.BorderMode = Enum.BorderMode.Outline
     ImageLabel.SizeConstraint = Enum.SizeConstraint.RelativeXY
-    ImageLabel.ZIndex = 1
+    ImageLabel.ZIndex = 2
     ImageLabel.BorderSizePixel = 0
     ImageLabel.Visible = true
     ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     ImageLabel.BackgroundTransparency = 1
     ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
     ImageLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
-    ImageLabel.Size = UDim2.new(0.061162080615758896, 0, 0.15, 0)
+    ImageLabel.Size = UDim2.new(0.25, 0, 0.4000000059604645, 0)
     ImageLabel.Image = "rbxassetid://115232362040752"
     ImageLabel.ImageTransparency = 1
 
     local Frame2 = Instance.new("Frame")
-    Frame2.Parent = ScreenGui
+    Frame2.Parent = Frame
     Frame2.LayoutOrder = 0
     Frame2.Active = false
     Frame2.Interactable = true
-    Frame2.AnchorPoint = Vector2.new(0.5, 0.5)
+    Frame2.AnchorPoint = Vector2.new(0, 0)
     Frame2.Rotation = 0
     Frame2.AutomaticSize = Enum.AutomaticSize.None
     Frame2.Name = ""
     Frame2.BorderMode = Enum.BorderMode.Outline
     Frame2.SizeConstraint = Enum.SizeConstraint.RelativeXY
-    Frame2.ZIndex = 1
+    Frame2.ZIndex = 2
     Frame2.BorderSizePixel = 0
     Frame2.Visible = true
     Frame2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Frame2.BackgroundTransparency = 1
     Frame2.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    Frame2.Position = UDim2.new(0.53, 0, 0.5, 0)
-    Frame2.Size = UDim2.new(0.12232416123151779, 0, 0.08389262109994888, 0)
+    Frame2.Position = UDim2.new(0.375, 0, 0.30000001192092896, 0)
+    Frame2.Size = UDim2.new(0.5, 0, 0.4000000059604645, 0)
     Frame2.ClipsDescendants = true
-
-    local UIAspectRatioConstraint2 = Instance.new("UIAspectRatioConstraint")
-    UIAspectRatioConstraint2.Name = ""
-    UIAspectRatioConstraint2.Parent = Frame
-    UIAspectRatioConstraint2.AspectRatio = 4
 
     local TextLabel = Instance.new("TextLabel")
     TextLabel.Parent = Frame2
@@ -107,62 +99,194 @@ local function Init(SafeUi, Cloneref)
     TextLabel.Name = ""
     TextLabel.BorderMode = Enum.BorderMode.Outline
     TextLabel.SizeConstraint = Enum.SizeConstraint.RelativeXY
-    TextLabel.ZIndex = 1
+    TextLabel.ZIndex = 2
     TextLabel.BorderSizePixel = 0
     TextLabel.Visible = true
     TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     TextLabel.BackgroundTransparency = 1
     TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    TextLabel.Position = UDim2.new(-1, 0, 0, 0)
-    TextLabel.Size = UDim2.new(1, 0, 1, 0)
+    TextLabel.Position = UDim2.new(-1, 0, 0.25, 0)
+    TextLabel.Size = UDim2.new(1, 0, 0.5, 0)
     TextLabel.FontFace = Font.new("rbxassetid://12187365364")
     TextLabel.RichText = true
     TextLabel.Text = "Meowsploit"
     TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-    TextLabel.TextSize = 40
-    
+    TextLabel.TextSize = 43
+
+    local UIGradient = Instance.new("UIGradient")
+    UIGradient.Parent = TextLabel
+    UIGradient.Rotation = 0
+    UIGradient.Name = ""
+    UIGradient.Rotation = 0
+
+    local Frame3 = Instance.new("Frame")
+    Frame3.Parent = Frame
+    Frame3.LayoutOrder = 0
+    Frame3.Active = false
+    Frame3.Interactable = true
+    Frame3.AnchorPoint = Vector2.new(0.5, 0.5)
+    Frame3.Rotation = 0
+    Frame3.AutomaticSize = Enum.AutomaticSize.None
+    Frame3.Name = ""
+    Frame3.BorderMode = Enum.BorderMode.Outline
+    Frame3.SizeConstraint = Enum.SizeConstraint.RelativeXY
+    Frame3.ZIndex = 2
+    Frame3.BorderSizePixel = 0
+    Frame3.Visible = true
+    Frame3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame3.BackgroundTransparency = 1
+    Frame3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Frame3.Position = UDim2.new(0.5, 0, 0.21199999749660492, 0)
+    Frame3.Size = UDim2.new(0.875, 0, 0.019999999552965164, 0)
+
+    local UICorner2 = Instance.new("UICorner")
+    UICorner2.Parent = Frame3
+    UICorner2.Name = ""
+    UICorner2.CornerRadius = UDim.new(1.600000023841858, 0)
+
+    local UIGradient2 = Instance.new("UIGradient")
+    UIGradient2.Parent = Frame3
+    UIGradient2.Rotation = 0
+    UIGradient2.Name = ""
+    UIGradient2.Rotation = 0
+
+    local UIShadow2 = Instance.new("UIShadow")
+    UIShadow2.Parent = Frame3
+    UIShadow2.Name = ""
+    UIShadow2.ZIndex = -1
+    UIShadow2.BlurRadius = UDim.new(0, 20)
+    UIShadow2.Color = Color3.fromRGB(255, 98, 98)
+    UIShadow2.Transparency = 1
+
+    local Frame4 = Instance.new("Frame")
+    Frame4.Parent = Frame
+    Frame4.LayoutOrder = 0
+    Frame4.Active = false
+    Frame4.Interactable = true
+    Frame4.AnchorPoint = Vector2.new(0.5, 0.5)
+    Frame4.Rotation = 0
+    Frame4.AutomaticSize = Enum.AutomaticSize.None
+    Frame4.Name = ""
+    Frame4.BorderMode = Enum.BorderMode.Outline
+    Frame4.SizeConstraint = Enum.SizeConstraint.RelativeXY
+    Frame4.ZIndex = 2
+    Frame4.BorderSizePixel = 0
+    Frame4.Visible = true
+    Frame4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame4.BackgroundTransparency = 1
+    Frame4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Frame4.Position = UDim2.new(0.5, 0, 0.7879999876022339, 0)
+    Frame4.Size = UDim2.new(0.875, 0, 0.019999999552965164, 0)
+
+    local UICorner3 = Instance.new("UICorner")
+    UICorner3.Parent = Frame4
+    UICorner3.Name = ""
+    UICorner3.CornerRadius = UDim.new(1.600000023841858, 0)
+
+    local UIShadow3 = Instance.new("UIShadow")
+    UIShadow3.Parent = Frame4
+    UIShadow3.Name = ""
+    UIShadow3.ZIndex = -1
+    UIShadow3.BlurRadius = UDim.new(0, 20)
+    UIShadow3.Color = Color3.fromRGB(255, 98, 98)
+    UIShadow3.Transparency = 1
+
+    local UIGradient3 = Instance.new("UIGradient")
+    UIGradient3.Parent = Frame4
+    UIGradient3.Rotation = 0
+    UIGradient3.Name = ""
+    UIGradient3.Rotation = 0
+
+    local UIGradient4 = Instance.new("UIGradient")
+    UIGradient4.Parent = Frame
+    UIGradient4.Rotation = 90
+    UIGradient4.Name = ""
+    UIGradient4.Rotation = 90
+
+    local ImageLabel2 = Instance.new("ImageLabel")
+    ImageLabel2.Parent = Frame
+    ImageLabel2.LayoutOrder = 0
+    ImageLabel2.Active = false
+    ImageLabel2.Interactable = true
+    ImageLabel2.AnchorPoint = Vector2.new(0.5, 0.5)
+    ImageLabel2.Rotation = 90
+    ImageLabel2.AutomaticSize = Enum.AutomaticSize.None
+    ImageLabel2.Name = ""
+    ImageLabel2.BorderMode = Enum.BorderMode.Outline
+    ImageLabel2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+    ImageLabel2.ZIndex = 0
+    ImageLabel2.BorderSizePixel = 0
+    ImageLabel2.Visible = true
+    ImageLabel2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    ImageLabel2.BackgroundTransparency = 1
+    ImageLabel2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    ImageLabel2.Position = UDim2.new(0.5, 0, 0.5, 0)
+    ImageLabel2.Size = UDim2.new(0.6175000071525574, 0, 1.5880000591278076, 0)
+    ImageLabel2.Image = "rbxassetid://128396430862819"
+    ImageLabel2.ImageColor3 = Color3.fromRGB(255, 73, 73)
+    ImageLabel2.ImageTransparency = 1
+
+    local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+    UIAspectRatioConstraint.Parent = Frame
+    UIAspectRatioConstraint.Name = ""
+    UIAspectRatioConstraint.AspectRatio = 1.600000023841858
+
     local TweenService = Cloneref(game:GetService("TweenService"))
     local function Tween(Inst, Props, t, ti)
         return TweenService:Create(Inst, ti or TweenInfo.new(t or 1), Props)
     end
 
-    local T1 = Tween(Frame, {BackgroundTransparency=0}, 1.5)
-    local T2 = Tween(UIShadow, {Transparency=0.5}, 1.5)
-    local T3 = Tween(ImageLabel, {ImageTransparency=0})
-    local T4 = Tween(ImageLabel, {Position=UDim2.new(0.44, 0, ImageLabel.Position.Y.Scale, 0)})
-    local T5 = Tween(TextLabel, {Position=UDim2.new(0, 0, 0, 0)})
-    local T6 = Tween(Frame, {BackgroundTransparency=1})
-    local T7 = Tween(ImageLabel, {ImageTransparency=1})
-    local T8 = Tween(TextLabel, {TextTransparency=1})
-    local T9 = Tween(UIShadow, {Transparency=1})
+    local Instructions = {
+        [1] = {"Tween", Tween(Frame, {BackgroundTransparency=1}, 1)},
+        [2] = {"Tween", Tween(UIShadow, {Transparency=0.3}, 1)},
+        [3] = {"Tween", Tween(ImageLabel2, {ImageTransparency=0.9})}
+        [4] = {"Tween", Tween(UIStroke, {Transparency=0}, 1)}
+        [5] = {"Function", function()
+            for i = 1, 150 do
+                UIGradient.Color = ColorSequence.new({
+                    ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 255, 255)),
+                    ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 255, 255)),
+                    ColorSequenceKeypoint.new(1, Color3.fromRGB(i, i, i))
+                })
+                task.wait(1/150)
+            end
+        end},
+        [6] = {"CompleteTween", 4},
+        [7] = {"Tween", Tween(ImageLabel, {ImageTransparency=0}, 1)},
+        [8] = {"Tween", Tween(Frame3, {BackgroundTransparency=0}, 1)},
+        [9] = {"Tween", Tween(Frame4, {BackgroundTransparency=0}, 1)},
+        [10] = {"Tween", Tween(UIShadow2, {Transparency=0.5}, 1)},
+        [11] = {"Tween", Tween(UIShadow3, {Transparency=0.5}, 1)}
+        [12] = {"CompleteTween", 11},
+        [13] = {"Tween", Tween(ImageLabel, {Position=UDim2.new(0.25,0,0.5,0)}, 1)},
+        [14] = {"CompleteTween", 13},
+        [15] = {"Tween", Tween(TextLabel, {Position=UDim2.new(0, 0, 0.25, 0)}, 1)},
+        [16] = {"CompleteTween", 15},
+        [17] = {"Function", function()
+            for i = 0, 30 do
+                local Color = Color3.fromRGB(0, 0, 0):Lerp(Color3.fromRGB(212, 68, 68), i / 30)
+                TextLabel.Text = string.format('Meow<font color="%s">sploit</font>', string.format("#%02x%02x%02x", Color.R * 255, Color.G * 255, Color.B * 255))
+                task.wait(1/30)
+            end
+        end},
+        [18] = {"Wait", 2},
+        [19] = {"Tween", Tween(ImageLabel, {ImageTransparency=1}, 1)},
+        [20] = {"Tween", Tween(TextLabel, {TextTransparency=1}, 1)},
+        [21] = {"Tween", Tween(Frame3, {BackgroundTransparency=1}, 1)},
+        [22] = {"Tween", Tween(Frame4, {BackgroundTransparency=1}, 1)},
+        [23] = {"Tween", Tween(UIShadow2, {Transparency=1}, 1)},
+        [24] = {"Tween", Tween(UIShadow3, {Transparency=1}, 1)},
+        [25] = {"Tween", Tween(ImageLabel2, {ImageTransparency=1}, 1)},
+        [26] = {"Tween", Tween(UIStroke, {Transparency=1}, 1)},
+        [27] = {"Tween", Tween(UIShadow, {Transparency=1}, 1)},
+        [28] = {"Tween", Tween(Frame, {BackgroundTransparency=1}, 1)},
+        [29] = {"CompleteTween", 28},
+        [30] = {"Function", function()
+            ScreenGui:Destroy()
+        end}
+    }
 
-    T1:Play()
-    T2:Play()
-    for i = 1, 74 do
-        UIGradient.Color = ColorSequence.new({
-            ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 255, 255)),
-            ColorSequenceKeypoint.new(1, Color3.fromRGB(255 - i, 255 - i, 255 - i))
-        })
-        task.wait()
-    end
-    T2.Completed:Wait()
-    T3:Play()
-    T3.Completed:Wait()
-    T4:Play()
-    T4.Completed:Wait()
-    T5:Play()
-    T5.Completed:Wait()
-    for i = 0, 30 do
-        local Color = Color3.fromRGB(0, 0, 0):Lerp(Color3.fromRGB(212, 68, 68), i / 30)
-        TextLabel.Text = string.format('Meow<font color="%s">sploit</font>', string.format("#%02x%02x%02x", Color.R * 255, Color.G * 255, Color.B * 255))
-        task.wait(1 / 30)
-    end
-    task.wait(2)
-    for _, Tween in {T6, T7, T8, T9} do
-        Tween:Play()
-    end
-    T9.Completed:Wait()
-    ScreenGui:Destroy()
+    Interpreter.Interpret(Instructions)
 end
 
 return {Init = Init}
--- Init.lua
+++ Init.lua
@@ -1,40 +1,33 @@
-if not game then --Use a dynamic package path for running on a lua environment
+local Acquire = require
+if not game then --Use a dynamic package path for running on vscode
     pcall(function()
         local PathInitDir = debug.getinfo(1, "S").source:sub(2):match("^(.*)[/\\]")
         package.path=PathInitDir.."/?.lua;"..PathInitDir.."/?/init.lua;"..package.path
     end)
+else
+    Acquire = function(Path)
+        return loadstring(game:HttpGet("https://raw.githubusercontent.com/im-fair/Meowsploit/refs/heads/main/" .. Path .. ".lua"))()
+    end
 end
 
 local function GetFuncFallback(Func, Fallback)
-    local Success = (pcall(function()
-        return getfenv()[Func]
-    end))
+    local Function = getfenv()[Func]
 
-    if Success then
-        return Func
+    if Function then
+        return Function
     else
         return Fallback or function(...) return ... end
     end
 end
 
 local RequiredFunctions = {
-    {"hookfunction", false},
-    {"cloneref", true},
-    {"gethui", true},
-    {"getgenv", true},
-    {"loadstring", false}
 }
 local LimitedFunctions = {}
 
-for _, FunctionData in RequiredFunctions do
-    if not getfenv()[FunctionData[1]] and not FunctionData[2] then
+for _, FunctionKey in RequiredFunctions do
+    if not getfenv()[FunctionKey] then
         table.insert(LimitedFunctions, FunctionData[1])
     end
-end
-
---Required to run with my architecture
-if table.find(LimitedFunctions, "hookfunction") or table.find(LimitedFunctions, "loadstring") then
-    error("Your executor is not compatible with Meowsploit's architecture!")
 end
 
 --Function Fallbacks
@@ -42,15 +35,36 @@ local cloneref = GetFuncFallback(cloneref)
 local gethui = GetFuncFallback(gethui, function()
     return cloneref(game:GetService("CoreGui"))
 end)
-local genv = {}; local getgenv = GetFuncFallback(getgenv, function(...) return genv end)
+local genv = {}; local getgenv = GetFuncFallback(getgenv, function() return genv end)
 
-if game then --Hook require locally to get code blobs from github instead of a sandboxed environment like vscode
-    local OldRequire; OldRequire = hookfunction(require, function(Path)
-        if not checkcaller() then return OldRequire(Path) end
+--Important
+local function Interpret(Instructions)
+    local ActiveTweens = {}
 
-        return loadstring(game:HttpGet("https://raw.githubusercontent.com/im-fair/Meowsploit/refs/heads/main/" .. Path .. ".lua"))()
-    end)
+    for Index = 1, #Instructions do
+        local Instruction = Instructions[Index]
+        local Type = Instruction[1]
+
+        if Type == "Tween" then
+            local TweenObject = Instruction[2]
+
+            if TweenObject then
+                ActiveTweens[Index] = TweenObject
+                TweenObject:Play()
+            end
+        elseif Type == "Function" then
+            task.spawn(Instruction[2])
+        elseif Type == "CompleteTween" then
+            local TweenObject = ActiveTweens[Instruction[2]]
+
+            if TweenObject then
+                TweenObject.Completed:Wait()
+            end
+        elseif Type == "Wait" then
+            task.wait(tonumber(Instruction[2]))
+        end
+    end
 end
 
 --UI Init
-require("Footer/Attribution").Init(gethui(), cloneref)
+Acquire("Footer/Attribution").Init(gethui(), cloneref, {Interpret = Interpret})
